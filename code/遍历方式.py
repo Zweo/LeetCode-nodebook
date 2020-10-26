a9c9df7aabe828e1e2a860339c10ff28740a04ff
Zweo <exzhouwei@outlook.com>
2020-10-26 23:34:01 +0800

    1

--- code/遍历方式.py
+++ code/遍历方式.py
@@ -1,4 +1,4 @@
-  def traverse(root: TreeNode):
+def traverse(root: TreeNode):
     if not root:
         return
     # 前序遍历
@@ -6,8 +6,9 @@
     # 中序遍历
     traverse(root.right)
     # 后序遍历
-    
- def path(root: TreeNode):
+
+
+def path(root: TreeNode):
     '''层次遍历'''
     queue = [root]
     while queue:
@@ -15,27 +16,29 @@
         queue.append(root.left) if root.left else None
         queue.append(root.right) if root.right else None
         print(cur.val)
-        
- def inorderTraversal(self, root: TreeNode):
-      '''迭代形式中序遍历'''
-      res = []
-      track = [root]
-      while track:
-          cur = track.pop()
-          if isinstance(cur, TreeNode):
-              track.extend([cur.right, cur.val, cur.left])
-          elif isinstance(cur, int):
-              res.append(cur)
-      return res
-      
- def postorderTraversal(self, root: TreeNode):
-      '''迭代形式后序遍历'''
-      res = []
-      track = [root]
-      while track:
-          cur = track.pop()
-          if isinstance(cur, TreeNode):
-              track.extend(cur.val, [cur.right,  cur.left])
-          elif isinstance(cur, int):
-              res.append(cur)
-      return res
+
+
+def inorderTraversal(self, root: TreeNode):
+    '''迭代形式中序遍历'''
+    res = []
+    track = [root]
+    while track:
+        cur = track.pop()
+        if isinstance(cur, TreeNode):
+            track.extend([cur.right, cur.val, cur.left])
+        elif isinstance(cur, int):
+            res.append(cur)
+    return res
+
+
+def postorderTraversal(self, root: TreeNode):
+    '''迭代形式后序遍历'''
+    res = []
+    track = [root]
+    while track:
+        cur = track.pop()
+        if isinstance(cur, TreeNode):
+            track.extend(cur.val, [cur.right, cur.left])
+        elif isinstance(cur, int):
+            res.append(cur)
+    return res
